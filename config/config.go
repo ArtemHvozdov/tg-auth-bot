66c19cab7b6747993a2551347b3341e341f1e88f
ArtemHvozdov <naltuhov4@gmail.com>
2025-01-21 01:54:18 +0200

    change import env var. Logic of bots is working, but not work logs in console. I think that Loader env var blockek work

--- config/config.go
+++ config/config.go
@@ -9,19 +9,37 @@ import (
 
 type Config struct {
 	TelegramToken string
+    InfuraKey string
+    NgrokURL string
 }
 
 func LoadConfig() Config {
-
-
+    // Downloading environment variables from .env file
 	if err := godotenv.Load(); err != nil {
         log.Println("No .env file found")
     }
+
+    // Getting the Telegram token
     token := os.Getenv("TELEGRAM_TOKEN")
     if token == "" {
         panic("TELEGRAM_BOT_TOKEN is not set")
     }
+
+    // Getting the Infura key
+    infuraKey := os.Getenv("INFURA_KEY")
+    if infuraKey == "" {
+        panic("INFURA_KEY is not set")
+    }
+
+    // Getting the Ngrok URL
+    ngrokURL := os.Getenv("NGROK_URL")
+    if ngrokURL == "" {
+        panic("NGROK_URL is not set")
+    }
+
     return Config{
         TelegramToken: token,
+        InfuraKey: infuraKey,
+        NgrokURL: ngrokURL,
     }
 }
